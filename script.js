0d7aa60df51502f88ee2e48e2468c33234685860
Caio Neme <caiocesarneme@gmail.com>
2023-04-12 10:55:11 -0300

    esta-colocando-o-usuario-na-lista-de-usuarios-do-sever

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+axios.defaults.headers.common['Authorization'] = 'ttqJn7RegtZYwPr8fvIDN6Fx';
+
 let User;
-let listUser = [];
+let listUser = {};
 let chat = document.querySelector('.chat');
 let agora = new Date();
 let hora = agora.getHours();
@@ -7,25 +9,59 @@ let minutos = agora.getMinutes();
 let segundos = agora.getSeconds();
 let tempo = `${hora.toString().padStart(2, '0')}:${minutos.toString().padStart(2, '0')}:${segundos.toString().padStart(2, '0')}`;
 let destinatario = 'Todos'
+let mensagens = axios.get('https://mock-api.driven.com.br/api/vm/uol/messages');
 
+function tratarSucesso(resposta){
+    console.log(resposta.data);
+    alert(`Seja bem-vindo, ${User}`)
+
+}
+
+function TratarErro(erro){
+    console.log(resposta.data);
+    alert(`O nome de usuario: ${User}, não esta disponivel por favor digite outro`);
+    User = prompt('Qual é o seu nome?');
+}
 
 function nameUser(){
     User = prompt('Qual é o seu nome?');
+    listUser = {name : User}
+    const promessa = axios.post('https://mock-api.driven.com.br/api/vm/uol/participants', listUser);
+    promessa.then(tratarSucesso);
+    promessa.catch(TratarErro);
 
-    for(c = 0; c <= listUser; c++){
-        if(User === listUser[c]){
-            alert(`O nome de usuario: ${User}, não esta disponivel por favor digite outro`);
-            User = prompt('Qual é o seu nome?');
-        }
-        else{
-            listUser.push(User);
-            alert(`Seja bem-vindo, ${User}`)
-        }
-    }
+    agora = new Date();
+    hora = agora.getHours();
+    minutos = agora.getMinutes();
+    segundos = agora.getSeconds();
+    tempo = `${hora.toString().padStart(2, '0')}:${minutos.toString().padStart(2, '0')}:${segundos.toString().padStart(2, '0')}`;
+
     chat.innerHTML += `
     <li class="mensagem entrada">
         <p>(${tempo})  <strong>${User}</strong>  entrou na sala...</p>
     </li>`
+}
+
+
+function loadError(){
+    console.log('erro ao carregar as mensagens');
+}
+
+function loadChat(){
+    let mensagens = axios.get('https://mock-api.driven.com.br/api/vm/uol/messages');
+    mensagens.then(loadOk);
+    mensagens.catch(loadError);
+
+    function loadOk(ok){
+        console.log(ok.data);
+        for(c = 0; c < 100; c++){
+            chat.innerHTML+= `
+                <li class="mensagem">
+                    <p>(${mensagens[c].time}) <strong>${mensagens[c].from}</strong> para <strong>${mensagens[c].to}</strong>: ${mensagens[c].text}</p>
+                </li>`
+        }
+    }
+    
 }
 
 function addMsn(){
@@ -44,4 +80,5 @@ function addMsn(){
             </li>`
 }
 
+loadChat();
 nameUser();
